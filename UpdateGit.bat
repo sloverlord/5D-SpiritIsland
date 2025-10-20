f0b127000efee96041163800d901a1454f868bb8
sloverlord <6453262+sloverlord@users.noreply.github.com>
2025-10-20 18:56:59 -0400

    les go

--- UpdateGit.bat
+++ UpdateGit.bat
@@ -2,16 +2,11 @@
 pushd %~dp0
 cscript createTimelines.vbs
 
-<<<<<<< HEAD
 git checkout -b img main
 git add *
 git commit -m "les go"
+git push
 
 git checkout main
 git merge img
 git branch -d img
-=======
-git add *
-git commit -a -m "les go"
-git push
->>>>>>> img
